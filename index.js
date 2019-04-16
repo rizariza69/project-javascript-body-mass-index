3b0000ee8cd3434354a1c7c3c804731889891563
Muhammad Riza <riza.riza69@gmail.com>
2019-04-16 10:45:34 +0700

    improve index.html(if-else)

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function show() {
   if (bmi < 18.5) {
     document.getElementById(
       "conclution"
-    ).innerHTML = `You thin, more eat and gym`;
+    ).innerHTML = `You thin severe thines, more eat and gym`;
   } else if (bmi < 24.9) {
     document.getElementById("conclution").innerHTML = `you normal, cool!`;
   } else if (bmi < 30) {
-    document.getElementById("conclution").innerHTML = `You must diet and gym`;
+    document.getElementById("conclution").innerHTML = `You Overweight, must diet and gym. keep health`;
+  } else if (bmi < 40) {
+    document.getElementById("conclution").innerHTML = `You Obese, you must extreme diet and gym. keep health`;
   }
 }
