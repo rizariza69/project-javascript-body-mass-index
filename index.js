881fee13f96bf8accba401ab75d4cd9d6d448399
Muhammad Riza <riza.riza69@gmail.com>
2019-04-16 10:26:51 +0700

    improve index.js and index.html

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-"use stirct"
-
+"use stirct";
 
 function show() {
-  const weight = document.getElementById('weight').value
-  const height = document.getElementById('height').value / 100
-  let bmi = weight / height ** 2
+
+  event.preventDefault()
+
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value / 100;
+  let bmi = weight / height ** 2;
 
   if (bmi < 18.5) {
-    document.getElementById('conclution').innerHTML = `You thin, more eat and gym`
+    document.getElementById(
+      "conclution"
+    ).innerHTML = `You thin, more eat and gym`;
   } else if (bmi < 24.9) {
-    document.getElementById('conclution').innerHTML = `you normal, cool!`
+    document.getElementById("conclution").innerHTML = `you normal, cool!`;
   } else if (bmi < 30) {
-    document.getElementById('conclution').innerHTML = `You must diet and gym`
+    document.getElementById("conclution").innerHTML = `You must diet and gym`;
   }
-
 }
